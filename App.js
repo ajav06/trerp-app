7789a68c25afc09d8ee3ede714684bfdac4977c8
AJAV06 <albert06av@outlook.com>
2021-07-20 10:21:30 -0400

    app

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const INJECTEDJAVASCRIPT = 'const meta = document.createElement(\'meta\'); meta.setAttribute(\'content\', \'width=device-width, initial-scale=1, maximum-scale=0.99, user-scalable=0\'); meta.setAttribute(\'name\', \'viewport\'); document.getElementsByTagName(\'head\')[0].appendChild(meta); '
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView source={{ uri: 'https://trodapp.com/' }} injectedJavaScript={INJECTEDJAVASCRIPT}/>
   );
 }
 
